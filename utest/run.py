4e4a0f36920d525f5a3b70df220b167ed056bf1d
Pekka Klärck <peke@iki.fi>
2013-12-28 12:25:54 +0200

    unit test runner

---
+++ utest/run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import abspath, dirname, join
+import os
+import sys
+import unittest
+
+
+curdir = dirname(abspath(__file__))
+sys.path.insert(0, join(dirname(curdir), 'src'))
+
+test_files = [f[:-3] for f in os.listdir(curdir)
+              if f.startswith('test_') and f.endswith('.py')]
+suite = unittest.defaultTestLoader.loadTestsFromNames(test_files)
+runner = unittest.TextTestRunner()
+result = runner.run(suite)
+rc = len(result.failures) + len(result.errors)
+sys.exit(rc)
