2a6659ad50ccb78ee616f7455d8f4d1d23741192
Robot Framework Developers <robotframework@gmail.com>
2014-02-27 16:07:25 +0200

    Update setup.py.
    - Read long description fromt README.rst.
    - Better mechanism to read version from source.
    - Etc.

--- setup.py
+++ setup.py
@@ -2,18 +2,9 @@
 
 from distutils.core import setup
 from os.path import abspath, dirname, join
+import re
 
-# Get __version__ dynamically
-execfile(join(dirname(abspath(__file__)), 'src', 'robotremoteserver.py'))
-
-DESCRIPTION = """
-Robot Framework remote servers allow hosting test libraries on different
-machines and/or interpreters than Robot Framework itself is running on.
-This version is implemented with Python and supports also Jython (JVM) and
-IronPython (.NET). Separate implementations exist for other languages.
-For more information about the remote library interface in general see
-http://code.google.com/p/robotframework/wiki/RemoteLibrary
-""".strip()
+NAME = 'robotremoteserver'
 CLASSIFIERS = """
 Development Status :: 5 - Production/Stable
 License :: OSI Approved :: Apache Software License
@@ -21,19 +12,25 @@ Operating System :: OS Independent
 Programming Language :: Python
 Topic :: Software Development :: Testing
 """.strip().splitlines()
+CURDIR = dirname(abspath(__file__))
+with open(join(CURDIR, 'src', NAME+'.py')) as source:
+    VERSION = re.search("\n__version__ = '(.*)'\n", source.read()).group(1)
+with open(join(CURDIR, 'README.rst')) as readme:
+    README = readme.read()
 
 setup(
-    name         = 'robotremoteserver',
-    version      = __version__,
-    author       = 'Robot Framework Developers',
-    author_email = 'robotframework@gmail.com',
-    url          = 'https://github.com/robotframework/PythonRemoteServer',
-    license      = 'Apache License 2.0',
-    description  = 'Robot Framework remote server',
-    long_description = DESCRIPTION,
-    keywords     = 'robotframework testing testautomation',
-    platforms    = 'any',
-    classifiers  = CLASSIFIERS,
-    package_dir  = {'': 'src'},
-    py_modules   = ['robotremoteserver']
+    name             = NAME,
+    version          = VERSION,
+    author           = 'Robot Framework Developers',
+    author_email     = 'robotframework@gmail.com',
+    url              = 'https://github.com/robotframework/PythonRemoteServer',
+    download_url     = 'https://pypi.python.org/pypi/robotremoteserver',
+    license          = 'Apache License 2.0',
+    description      = 'Python Remote Server for Robot Framework',
+    long_description = README,
+    keywords         = 'robotframework testing testautomation remote',
+    platforms        = 'any',
+    classifiers      = CLASSIFIERS,
+    package_dir      = {'': 'src'},
+    py_modules       = [NAME],
 )
