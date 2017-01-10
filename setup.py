63117cd8a3a253b9d421a3e4f12dc12088884e18
Pekka Klärck <peke@iki.fi>
2017-01-10 10:12:02 +0200

    setup.py enhancements
    - Use setuptools when available.
    - Update classifiers. Includes Python 3 support (#17).
    - Add me as author instead of generic RF Developers.

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 from os.path import abspath, dirname, join
 import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 NAME = 'robotremoteserver'
 CLASSIFIERS = '''
 Development Status :: 5 - Production/Stable
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
-Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+Programming Language :: Python :: Implementation :: CPython
+Programming Language :: Python :: Implementation :: Jython
+Programming Language :: Python :: Implementation :: IronPython
+Programming Language :: Python :: Implementation :: PyPy
 Topic :: Software Development :: Testing
+Framework :: Robot Framework
 '''.strip().splitlines()
 CURDIR = dirname(abspath(__file__))
 with open(join(CURDIR, 'src', NAME+'.py')) as source:
@@ -18,15 +29,16 @@ with open(join(CURDIR, 'src', NAME+'.py')) as source:
 with open(join(CURDIR, 'README.rst')) as readme:
     README = readme.read()
 
+
 setup(
     name             = NAME,
     version          = VERSION,
-    author           = 'Robot Framework Developers',
+    author           = u'Pekka Kl\xe4rck and contributors',
     author_email     = 'robotframework@gmail.com',
     url              = 'https://github.com/robotframework/PythonRemoteServer',
     download_url     = 'https://pypi.python.org/pypi/robotremoteserver',
     license          = 'Apache License 2.0',
-    description      = 'Python Remote Server for Robot Framework',
+    description      = 'Robot Framework remote server implemented with Python',
     long_description = README,
     keywords         = 'robotframework testing testautomation remoteinterface',
     platforms        = 'any',
